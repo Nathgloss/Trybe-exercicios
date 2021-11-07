b87d6564f5bf2526ca9854097a1f7f1e2c85b2d2
Nathgloss <nath.gall@icloud.com>
2021-11-07 12:02:07 -0300

    Alterações exercícios 2 e 3

--- fundamentos/bloco-4-javascript-e-logica/dia-4.1-primeiros-passos/Exercícios.js
+++ fundamentos/bloco-4-javascript-e-logica/dia-4.1-primeiros-passos/Exercícios.js
@@ -11,16 +11,20 @@ console.log(a % b);
 let x = 6;
 let y = 9;
 if (x > y) {
-    console.log(x);
+    console.log("'x' + é maior que + 'y'");
 } else {
-    console.log(y);
+    console.log("'y' + é maior que + 'x'");
 }
 
 //exercicio 3:
-let e = 8;
-let f = 9;
-let g = 3;
+const e = 8;
+const f = 9;
+const g = 11;
 
 if (e > f && e > g) {
-    console.log(e);
+    console.log('O número maior é ' + e + '(e)');
+} else if (f > e && f >g) {
+    console.log('O número maior é ' + f + '(f)');
+} else {
+    console.log('O número maior é ' + g + '(g)');
 }
