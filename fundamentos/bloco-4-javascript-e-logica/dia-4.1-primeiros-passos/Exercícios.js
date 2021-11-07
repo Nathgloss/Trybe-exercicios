56c68ecbf62b869b462ab1265d972c762801146b
Nathgloss <nath.gall@icloud.com>
2021-11-07 11:47:45 -0300

    Exercícios 1, 2 e 3

---
+++ fundamentos/bloco-4-javascript-e-logica/dia-4.1-primeiros-passos/Exercícios.js
@@ -0,0 +1,26 @@
+//exercicio 1:
+let a = 5;
+let b = 6;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+//exercicio 2:
+let x = 6;
+let y = 9;
+if (x > y) {
+    console.log(x);
+} else {
+    console.log(y);
+}
+
+//exercicio 3:
+let e = 8;
+let f = 9;
+let g = 3;
+
+if (e > f && e > g) {
+    console.log(e);
+}
