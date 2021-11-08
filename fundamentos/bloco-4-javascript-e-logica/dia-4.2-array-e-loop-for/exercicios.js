c69f72336eb0313e15b99278dbc0d6551083281a
Nathgloss <nath.gall@icloud.com>
2021-11-08 19:07:53 -0300

    Exercício 3

--- fundamentos/bloco-4-javascript-e-logica/dia-4.2-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-javascript-e-logica/dia-4.2-array-e-loop-for/exercicios.js
@@ -1,7 +1,22 @@
-//exercicio 1:
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for(let i = 0 ; i < numbers.length ; i++) {
+//exercício 1:
+for(i = 0 ; i < numbers.length ; i+=1) {
     console.log(numbers[i]);
 }
+
+//exercício 2:
+let somaNumbers = 0;
+for(i = 0 ; i < numbers.length ; i+=1) {
+        somaNumbers += numbers[i] ;
+} 
+console.log(somaNumbers);
+
+//exercício 3:
+let soma= 0;
+for(i = 0 ; i < numbers.length ; i+=1) {
+        soma += numbers[i] ;
+} 
+let mediaAritmetica = soma / numbers.length;
+console.log(mediaAritmetica);
+
